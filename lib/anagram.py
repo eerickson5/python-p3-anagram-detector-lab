4fcbc01be88bbd43f712c2fbb60eaa4260101045
Elise Erickson <elisece717@gmail.com>
2024-02-07 15:56:37 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import ipdb
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        self_array = sorted(list(self.word))
+        matches = []
+        words_as_arrays = [sorted(list(word)) for word in words]
+        for i in range(len(words_as_arrays)):
+            if words_as_arrays[i] == self_array:
+                matches.append(words[i])
+        return matches
